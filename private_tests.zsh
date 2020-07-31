2ceb6604d586124cde683571e55d4bf042a9a0d9
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-07-31 04:57:03 +0430

    updated docs

--- private_tests.zsh
+++ private_tests.zsh
@@ -1,1 +1,5 @@
 arr0 123Hello hi '' 123a '' 123 ''  12j ggg | cargo run  -- '123' '\x00' '\x00' |cat -v
+
+ec 'c\x00c' | rmprefix '' '\x00' 'HO'|cat -v
+
+ec 'c\x00c' | rmprefix '' 'c\x00' 'HO'|cat -v
--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,11 @@ fn main() {
 
 Usage: `rmprefix <prefix> [<input-separator>=\n] [<output-separator>=\n]`
 
-Set the separators to \x00 for the NUL character.
+Set the separators to `\x00` for the NUL character.
+
+`rmprefix` can also work as a search and replace tool, with this neat trick:
+
+`rmprefix '' SEARCH_FOR_ME REPLACE_WITH_ME`
 
 ## Examples
 
@@ -44,15 +48,27 @@ echo "The jungles are green.
 Some people sing.
 The world is not flat.
 There are two ways." | rmprefix The
-# =>
-# jungles are green.
-#Some people sing.
-# world is not flat.
-#re are two ways.
-
-echo "123456789" "23456789" "12345" "Hi" "" "Ocean" | rmprefix 123 ' ' '|||'
-# => 456789|||23456789|||45|||Hi||||||Ocean
 ```
+
+> jungles are green.
+>
+>Some people sing.
+>
+> world is not flat.
+>
+>re are two ways.
+
+```
+echo "123456789" "23456789" "12345" "Hi" "" "Ocean" | rmprefix 123 ' ' '|||'
+```
+
+> 456789|||23456789|||45|||Hi||||||Ocean
+
+```
+echo "Butterflies are insects in the macrolepidopteran clade Rhopalocera from the order Lepidoptera, which also includes moths. Adult butterflies have large, often brightly coloured wings, and conspicuous, fluttering flight. The group comprises the large superfamily Papilionoidea, which contains at least one former group, the skippers (formerly the superfamily "Hesperioidea"), and the most recent analyses suggest it also contains the moth-butterflies (formerly the superfamily "Hedyloidea"). Butterfly fossils date to the Paleocene, about 56 million years ago." | rmprefix '' butter cheese
+```
+
+> Butterflies are insects in the macrolepidopteran clade Rhopalocera from the order Lepidoptera, which also includes moths. Adult cheeseflies have large, often brightly coloured wings, and conspicuous, fluttering flight. The group comprises the large superfamily Papilionoidea, which contains at least one former group, the skippers (formerly the superfamily Hesperioidea), and the most recent analyses suggest it also contains the moth-cheeseflies (formerly the superfamily Hedyloidea). Butterfly fossils date to the Paleocene, about 56 million years ago.
 
 # Installation
 
