73bbb3f76b3af3d818f23ebc576f13b1610bb0f0
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-07-31 05:21:45 +0430

    micro changes

--- private_tests.zsh
+++ private_tests.zsh
@@ -3,3 +3,5 @@ arr0 123Hello hi '' 123a '' 123 ''  12j ggg | cargo run  -- '123' '\x00' '\x00' 
 ec 'c\x00c' | rmprefix '' '\x00' 'HO'|cat -v
 
 ec 'c\x00c' | rmprefix '' 'c\x00' 'HO'|cat -v
+
+ec "h.a..s" | sdlit . _._
